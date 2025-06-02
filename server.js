49e95a00426e968e9c97a7aa0da40c347c11945c
Zunw <43964753+ZunwDev@users.noreply.github.com>
2025-06-02 20:16:16 +0200

    feat: Add host to app

--- server.js
+++ server.js
@@ -5,7 +5,10 @@ dotenv.config();
 //import { scheduleUpdate } from "./src/scripts/updateGames.js";
 
 try {
-  await app.listen({ port: Number(process.env.PORT) || 3000 });
+  await app.listen({
+    port: Number(process.env.PORT) || 3000,
+    host: "0.0.0.0",
+  });
 
   // Enable when in production
   //scheduleUpdate();
